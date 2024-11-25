75e2de4d6a8ebdda3b88c76fcc15feea0eb5e973
Nethmina <159692690+NAWANJANA-LK@users.noreply.github.com>
2024-11-25 09:13:41 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,5 +1,5 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
+const GITHUB_AUTH_TOKEN = " ghp_AA5gZL25nfuHMpLvhBGUNv3MEF6tci0ZpJ2 6"
+const BOT_NUMBER = "94740092873"
 const SESSION_ID = ""
 
 
